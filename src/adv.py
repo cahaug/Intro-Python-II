60e5b337b62e6d953828a411a4a4f7b6991c8f30
cahaug <caleahaug@gmail.com>
2020-02-19 19:28:07 -0700

    day 1 mvp

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,5 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
@@ -24,21 +25,27 @@ earlier adventurers. The only exit is to the south."""),
 
 # Link rooms together
 
-room['outside'].n_to = room['foyer']
-room['foyer'].s_to = room['outside']
-room['foyer'].n_to = room['overlook']
-room['foyer'].e_to = room['narrow']
-room['overlook'].s_to = room['foyer']
-room['narrow'].w_to = room['foyer']
-room['narrow'].n_to = room['treasure']
-room['treasure'].s_to = room['narrow']
+room['outside'].go_North = room['foyer']
+room['foyer'].go_South = room['outside']
+room['foyer'].go_North = room['overlook']
+room['foyer'].go_East = room['narrow']
+room['overlook'].go_South = room['foyer']
+room['narrow'].go_West = room['foyer']
+room['narrow'].go_North = room['treasure']
+room['treasure'].go_South = room['narrow']
 
 #
 # Main
 #
 
 # Make a new player object that is currently in the 'outside' room.
-
+newPlayerObject = Player('player1', room['foyer'])
+# print(newPlayerObject.room)
+# print("\n You find yourself in the " + newPlayerObject + ".")
+print('Sunlight streaming onto your face wakes up, your head hurts.  Where are you?')
+# print("\n Enter a Cardinal Direction to move in that direction.")
+choice = str(input('\n Press Enter to Begin \n'))
+# print(choice)
 # Write a loop that:
 #
 # * Prints the current room name
@@ -49,3 +56,46 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+while choice != "q":
+    print("\n You find yourself in the " + newPlayerObject.currentRoom.name + ".")
+    choice = input("\n What Direction to go in? (N,S,E,W): \n").lower()
+    print(newPlayerObject.currentRoom)
+
+    # print("S "+str(hasattr(newPlayerObject.room, 'go_South')))
+    # print("N "+str(hasattr(newPlayerObject.room, 'go_North')))
+    # print("E "+str(hasattr(newPlayerObject.room, 'go_East')))
+    # print("W "+str(hasattr(newPlayerObject.room, 'go_West')))
+
+    if choice == 'n':
+        if hasattr(newPlayerObject.currentRoom, 'go_North'):
+            newPlayerObject.currentRoom = newPlayerObject.currentRoom.go_North
+            print('\n You walk to the north, and arrive at the ' + newPlayerObject.currentRoom.name + '. \n')
+            print(newPlayerObject.currentRoom.description + '\n')
+            print("What do we do now?")
+        else:
+            print('\n Nothing to see here. We should look elsewhere.')
+    elif choice == 's':
+        if hasattr(newPlayerObject.currentRoom, 'go_South'):
+            newPlayerObject.currentRoom = newPlayerObject.currentRoom.go_South
+            print('\n You walk to the south, and arrive at the ' + newPlayerObject.currentRoom.name + '. \n')
+            print(newPlayerObject.currentRoom.description + '\n')
+        else:
+            print('\n Nothing to see here. We should look elsewhere.')
+    elif choice == 'e':
+        if hasattr(newPlayerObject.currentRoom, 'go_East'):
+            newPlayerObject.currentRoom = newPlayerObject.currentRoom.go_East
+            print('\n You walk to the east, and arrive at the ' + newPlayerObject.currentRoom.name+ '. \n')
+            print(newPlayerObject.currentRoom.description + '\n')
+        else:
+            print('\n Nothing to see here. We should look elsewhere.')
+    elif choice == 'w':
+        if hasattr(newPlayerObject.currentRoom, 'go_West'):
+            newPlayerObject.currentRoom = newPlayerObject.currentRoom.go_West
+            print('\n You walk to the west, and arrive at the ' + newPlayerObject.currentRoom + '. \n')
+            print(newPlayerObject.currentRoom.description + '\n')
+        else:
+            print('\n Nothing to see here. We should look elsewhere.')
+    else:
+        print("Please Enter a Valid Direction, or enter Q to Quit.")
+
+print('\n Thanks for playing! \n')
--- src/player.py
+++ src/player.py
@@ -1,2 +1,8 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+class Player:
+    def __init__(self, name, currentRoom):
+        self.name = name
+        self.currentRoom = currentRoom
+    def __str__(self):
+        return "\n Player Name: " + self.name + "\n Player Location (Room): "+ self.currentRoom
