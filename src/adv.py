7b0b3a08f27b208c7bb7af38d6ea314a92ec4ff8
cahaug <caleahaug@gmail.com>
2020-02-20 18:56:31 -0700

    modified content of game, now working on day two mvp

--- src/adv.py
+++ src/adv.py
@@ -4,47 +4,87 @@ from player import Player
 # Declare all the rooms
 
 room = {
-    'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+    '1':  Room("in the center of the room", """A tattered rug adorns the floor.  I clasp my throbbing head.  Where am I?  What happened?  I can't remember.  This is scaring me, where am I, Where am I, WHERE AM I?"""),
 
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+    '2':  Room("by a dresser", """Dim light filters in from the east. You pull open the dresser drawers."""),
 
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
-into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+    '3':  Room("by a window", """In the bottom half of the window: the city at night-- twinkling lights flickering in the darkness. The tops of buildings. I'm on a high floor, but how did I get here? All I remember is being back at the diner and sipping my coffee.  How did I even get here?"""),
 
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+    '4':   Room("at the bed", """A soft spot for someone to rest their weary eyes, but not me.  I have to find out why I am here, and how to get back to where I came from."""),
 
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
-chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+    '5':   Room("by a black backpack leaning against the wall", """A black, open-mouthed Jansport is propped up, leaning against the wall.  I wonder what's inside."""),
+
+    '6':   Room("by a desk", """The bright shine of a login screen on a laptop illuminates the black Ikea desk in front of you.  The text screen on the screen says that this is Mr_Robot's MacBook.  Mr_Robot.  I wonder who that is."""),
+
+    '7':   Room("at the refrigerator", """Wow.  A refrigerator, and it's full.  What is going on here?"""),
+
+    '8':   Room("at the door", """Amazing.  A door.  Curses! It's locked, and a deadbolt at that. This will take a key to open.  A key, I wonder where I could find one?"""),
+
+    '9':   Room("in the kitchen", """A very modern stove, marble countertops.  Rent on this place must cost a fortune! There's a bowl on the counter."""),
+
+    '10':   Room("outside", """The key successfully opened the door. I escaped, and now am free.  Congratulations, I have won the game. And you player, you have just lost the game."""),
 }
 
+# Map of all of the rooms/possible moves
+#  -------------------------------- DOOR (10) ------
+# |    9    <->       7        <->        8         |
+# | kitchen          <->                 <->        |
+# |   5 bp  <->       1        <->        6   desk  |
+# |  <->             <->                 <->        |
+# |   4     <->       2        <->        3         |
+# |  bed           dresser              window      |
+#  -------------------------------------------------
 
 # Link rooms together
+room['1'].go_North = room['7']
+room['1'].go_South = room['2']
+room['1'].go_East = room['6']
+room['1'].go_West = room['5']
+room['2'].go_North = room['1']
+# room['2'].go_South = NONE
+room['2'].go_East = room['3']
+room['2'].go_West = room['4']
+room['3'].go_North = room['6']
+# room['3'].go_South = NONE
+# room['3'].go_East = NONE
+room['3'].go_West = room['2']
+room['4'].go_North = room['5']
+# room['4'].go_South = NONE
+room['4'].go_East = room['2']
+# room['4'].go_West = NONE
+room['5'].go_North = room['9']
+room['5'].go_South = room['4']
+room['5'].go_East = room['1']
+# room['5'].go_West = NONE
+room['6'].go_North = room['8']
+room['6'].go_South = room['3']
+# room['6'].go_East = NONE
+room['6'].go_West = room['1']
+# room['7'].go_North = NONE
+room['7'].go_South = room['1']
+room['7'].go_East = room['8']
+room['7'].go_West = room['9']
+# room['8'].go_North = NONE
+room['8'].go_South = room['6']
+# room['8'].go_East = NONE
+room['8'].go_West = room['7']
+# room['9'].go_North = NONE
+room['9'].go_South = room['5']
+room['9'].go_East = room['7']
+# room['9'].go_West = NONE
 
-room['outside'].go_North = room['foyer']
-room['foyer'].go_South = room['outside']
-room['foyer'].go_North = room['overlook']
-room['foyer'].go_East = room['narrow']
-room['overlook'].go_South = room['foyer']
-room['narrow'].go_West = room['foyer']
-room['narrow'].go_North = room['treasure']
-room['treasure'].go_South = room['narrow']
 
 #
 # Main
 #
 
 # Make a new player object that is currently in the 'outside' room.
-newPlayerObject = Player('player1', room['foyer'])
+newPlayerObject = Player('player1', room['1'])
 # print(newPlayerObject.room)
 # print("\n You find yourself in the " + newPlayerObject + ".")
 print('Sunlight streaming onto your face wakes up, your head hurts.  Where are you?')
 # print("\n Enter a Cardinal Direction to move in that direction.")
-choice = str(input('\n Press Enter to Begin \n'))
+choice = str(input('Press Enter to Begin'))
 # print(choice)
 # Write a loop that:
 #
@@ -57,9 +97,9 @@ choice = str(input('\n Press Enter to Begin \n'))
 #
 # If the user enters "q", quit the game.
 while choice != "q":
-    print("\n You find yourself in the " + newPlayerObject.currentRoom.name + ".")
-    choice = input("\n What Direction to go in? (N,S,E,W): \n").lower()
-    print(newPlayerObject.currentRoom)
+    print("You are " + newPlayerObject.currentRoom.name + ".")
+    choice = input("What Direction to go in? (N,S,E,W): ").lower()
+    # print(newPlayerObject.currentRoom)
 
     # print("S "+str(hasattr(newPlayerObject.room, 'go_South')))
     # print("N "+str(hasattr(newPlayerObject.room, 'go_North')))
@@ -69,32 +109,31 @@ while choice != "q":
     if choice == 'n':
         if hasattr(newPlayerObject.currentRoom, 'go_North'):
             newPlayerObject.currentRoom = newPlayerObject.currentRoom.go_North
-            print('\n You walk to the north, and arrive at the ' + newPlayerObject.currentRoom.name + '. \n')
-            print(newPlayerObject.currentRoom.description + '\n')
-            print("What do we do now?")
+            print('You walk to the north.')
+            print(newPlayerObject.currentRoom.description)
         else:
-            print('\n Nothing to see here. We should look elsewhere.')
+            print("""Can't go North. There's a wall in my way.""")
     elif choice == 's':
         if hasattr(newPlayerObject.currentRoom, 'go_South'):
             newPlayerObject.currentRoom = newPlayerObject.currentRoom.go_South
-            print('\n You walk to the south, and arrive at the ' + newPlayerObject.currentRoom.name + '. \n')
-            print(newPlayerObject.currentRoom.description + '\n')
+            print('You walk to the south.')
+            print(newPlayerObject.currentRoom.description)
         else:
-            print('\n Nothing to see here. We should look elsewhere.')
+            print("""Can't go South. There's a wall in my way.""")
     elif choice == 'e':
         if hasattr(newPlayerObject.currentRoom, 'go_East'):
             newPlayerObject.currentRoom = newPlayerObject.currentRoom.go_East
-            print('\n You walk to the east, and arrive at the ' + newPlayerObject.currentRoom.name+ '. \n')
-            print(newPlayerObject.currentRoom.description + '\n')
+            print('You walk to the east.')
+            print(newPlayerObject.currentRoom.description)
         else:
-            print('\n Nothing to see here. We should look elsewhere.')
+            print("""Can't go East. There's a wall in my way.""")
     elif choice == 'w':
         if hasattr(newPlayerObject.currentRoom, 'go_West'):
             newPlayerObject.currentRoom = newPlayerObject.currentRoom.go_West
-            print('\n You walk to the west, and arrive at the ' + newPlayerObject.currentRoom + '. \n')
-            print(newPlayerObject.currentRoom.description + '\n')
+            print('You walk to the west.')
+            print(newPlayerObject.currentRoom.description)
         else:
-            print('\n Nothing to see here. We should look elsewhere.')
+            print("""Can't go West. There's a wall in my way.""")
     else:
         print("Please Enter a Valid Direction, or enter Q to Quit.")
 
--- src/room.py
+++ src/room.py
@@ -4,5 +4,9 @@ class Room:
     def __init__(self, name, description):
         self.name = name
         self.description = description
+        self.go_North = None
+        self.go_South = None
+        self.go_East = None
+        self.go_West = None
     def __str__(self):
         return '\n Room Name: ' + self.name + '\n Description: ' + self.description
